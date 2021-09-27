774753471cf7be8b18dc19dec8478f01d35a8a82
jisu <jisu.han3201@gmail.com>
2021-09-27 19:30:01 +0900

    done bridge and composite pattern

---
+++ composite/neuralnetworks/neuralnetworks.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type NeuronInterface interface {
+	Iter() []*Neuron
+}
+
+type Neuron struct {
+	In, Out []*Neuron
+}
+
+func (n *Neuron) Iter() []*Neuron {
+	return []*Neuron{n}
+}
+
+func (n *Neuron) ConnectTo(other *Neuron) {
+	n.Out = append(n.Out, other)
+	other.In = append(other.In, n)
+}
+
+type NeuronLayer struct {
+	Neurons []Neuron
+}
+
+func (n *NeuronLayer) Iter() []*Neuron {
+	result := make([]*Neuron, 0)
+	for i := range n.Neurons {
+		result = append(result, &n.Neurons[i])
+	}
+	return result
+}
+
+func NewNeuronLayer(count int) *NeuronLayer {
+	return &NeuronLayer{make([]Neuron, count)}
+}
+
+func Connect(left, right NeuronInterface) {
+	for _, l := range left.Iter() {
+		for _, r := range right.Iter() {
+			l.ConnectTo(r)
+		}
+	}
+}
+
+func main() {
+	neuron1, neuron2 := &Neuron{}, &Neuron{}
+	layer1, layer2 := NewNeuronLayer(3), NewNeuronLayer(4)
+
+	// Neuron, NeuronLayer 모두 Connect 함수 하나로 연결하고 싶은 경우
+	// 인터페이스를 이용하여 구현
+	Connect(neuron1, neuron2)
+	Connect(neuron1, layer1)
+	fmt.Println(neuron1)
+	Connect(layer2, neuron1)
+	Connect(layer1, layer2)
+	fmt.Println(layer1, layer2, neuron1, neuron2)
+}
